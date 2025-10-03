6df2e5ed7fa3f65940a8d3dc76b1559c0b0aa2b7
Shiva <shivarathore282004@gmail.com>
2025-10-03 23:37:10 +0530

    used dotenv effectively

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const JWT_USER_PASSWORD = "useraladin";
-const JWT_ADMIN_PASSWORD = "adminaladin";
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
 
 
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require ("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -21,7 +22,7 @@ app.use("/admin", adminRouter);
 async function main() {
 
 
-   await mongoose.connect("");
+   await mongoose.connect(process.env.MONGODB_URL);
     console.log("conntected to mongoose")
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
